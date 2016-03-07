27d23d41bcf69de5067a1f64ced265224c61b666
Carlos Arnaud <c.arnaudmartin@gmail.com>
2016-03-07 13:00:52 +0100

    C:/Program Files (x86)/Git/time completado. Closes #10

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ var app = express();
 app.use("/", express.static(__dirname + "/static"));
 
 app.get("/time", (req,res)=>{
+  days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   var now = new Date();
-  res.send("It is:  "+now);
+  var d = now.getDay();
+  day = days[d];
+  var hour = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+  res.send("Today is  " + day + ", and are " + hour + ":" + minutes + ":" + seconds);
 });
 
 app.listen(port);
