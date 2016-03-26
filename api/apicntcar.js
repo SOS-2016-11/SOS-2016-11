1d0e5087a01cb6212ba9a325f3157708d431382f
Carlos Arnaud <c.arnaudmartin@gmail.com>
2016-03-26 16:45:24 +0100

    Busquedas hechas

--- api/apicntcar.js
+++ api/apicntcar.js
@@ -1,4 +1,4 @@
-// SANDBOX
+// SANDBOX ---------------------------------------------------------------------
 
 var programming_languages = [];
 
@@ -80,18 +80,33 @@ module.exports.loadInitialData = (req, res) => {
 var Basins = [];
 
 module.exports.getBasins = (req, res) => {
+  var limit = req.query.limit;
+  var offset = req.query.offset;
   var month = req.query.month;
   var year = req.query.year;
-  var pm = req.query.pm;
+/*  var pm = req.query.pm;
   var pe = req.query.pe;
-  var pa = req.query.pa;
-  if(month || year || pm || pe || pa){
+  var pa = req.query.pa;*/
+  if(month || year /*|| pm || pe || pa*/){
     var BasinsSeg = [];
     for (var i = 0; i < Basins.length; i++) {
-      if(Basins[i].month == month){
-        BasinsSeg.push(Basins[i]);
+      if(month){
+        if(Basins[i].month == month){
+          if(year){
+            if (Basins[i].year == year){
+              BasinsSeg.push(Basins[i]);
+            }
+          }else{
+            BasinsSeg.push(Basins[i]);
+          }
+        }
+      }else if(year){
+        if(Basins[i].year == year){
+              BasinsSeg.push(Basins[i]);
+        }
       }
     }
+
     res.send(BasinsSeg);
   }else{
     res.send(Basins);
@@ -99,9 +114,9 @@ module.exports.getBasins = (req, res) => {
 }
 
 module.exports.postBasins = (req, res) => {
-  var plb = req.body;
-  Basins.push(plb);
-  res.sendStatus(200);
+  var basinb = req.body;
+  Basins.push(basinb);
+  res.sendStatus(201);
 }
 
 module.exports.putBasins = (req, res) => {
@@ -115,30 +130,60 @@ module.exports.deleteBasins = (req, res) => {
 
 
 module.exports.getBasin = (req, res) => {
-  var pl = req.params.river_basin;
-
-
-  if(pl == "loadInitialData"){
+  var basin = req.params.river_basin;
+  if(basin == "loadInitialData"){
     Basins = [
-      {"river_basin": "Duero", "month": "Enero", "year": 2015, "p.m.": 56, "p.e.": 40.9, "p.a.": 301.9},
-      {"river_basin": "Tajo", "month": "Enero", "year": 2015, "p.m.": 60, "p.e.": 36.3, "p.a.": 345.6},
-      {"river_basin": "Guadiana", "month": "Enero", "year": 2015, "p.m.": 57, "p.e.": 34.2, "p.a.": 302.8},
-      {"river_basin": "Guadalquivir", "month": "Enero", "year": 2015, "p.m.": 68, "p.e.": 55.2, "p.a.": 331.2},
-      {"river_basin": "Ebro", "month": "Ener", "year": 2015, "p.m.": 44, "p.e.": 48.8, "p.a.": 306.9}
+      {"river_basin": "Duero", "month": "January", "year": 2015, "p.m.": 56, "p.e.": 40.9, "p.a.": 301.9},
+      {"river_basin": "Tajo", "month": "January", "year": 2015, "p.m.": 60, "p.e.": 36.3, "p.a.": 345.6},
+      {"river_basin": "Guadiana", "month": "January", "year": 2015, "p.m.": 57, "p.e.": 34.2, "p.a.": 302.8},
+      {"river_basin": "Guadalquivir", "month": "January", "year": 2015, "p.m.": 68, "p.e.": 55.2, "p.a.": 331.2},
+      {"river_basin": "Ebro", "month": "January", "year": 2015, "p.m.": 44, "p.e.": 48.8, "p.a.": 306.9},
+
+      {"river_basin": "Duero", "month": "January", "year": 2014, "p.m.": 56, "p.e.": 40.9, "p.a.": 301.9},
+      {"river_basin": "Tajo", "month": "January", "year": 2014, "p.m.": 60, "p.e.": 36.3, "p.a.": 345.6},
+      {"river_basin": "Guadiana", "month": "January", "year": 2014, "p.m.": 57, "p.e.": 34.2, "p.a.": 302.8},
+      {"river_basin": "Guadalquivir", "month": "January", "year": 2014, "p.m.": 68, "p.e.": 55.2, "p.a.": 331.2},
+      {"river_basin": "Ebro", "month": "January", "year": 2014, "p.m.": 44, "p.e.": 48.8, "p.a.": 306.9},
+
+      {"river_basin": "Duero", "month": "January", "year": 2013, "p.m.": 56, "p.e.": 40.9, "p.a.": 301.9},
+      {"river_basin": "Tajo", "month": "January", "year": 2013, "p.m.": 60, "p.e.": 36.3, "p.a.": 345.6},
+      {"river_basin": "Guadiana", "month": "January", "year": 2013, "p.m.": 57, "p.e.": 34.2, "p.a.": 302.8},
+      {"river_basin": "Guadalquivir", "month": "January", "year": 2013, "p.m.": 68, "p.e.": 55.2, "p.a.": 331.2},
+      {"river_basin": "Ebro", "month": "January", "year": 2013, "p.m.": 44, "p.e.": 48.8, "p.a.": 306.9}
     ];
     res.sendStatus(200);
   }else{
-    var mes = 404;
+    var cod = 404;
+    var limit = req.query.limit;
+    var offset = req.query.offset;
+    var month = req.query.month;
+    var year = req.query.year;
+    var BasinsSeg = [];
     for(var i = 0; i < Basins.length; i++){
-      if(Basins[i].river_basin == pl){
-        mes = Basins[i];
-        break;
+      if(Basins[i].river_basin == basin){
+        if(month){
+          if(Basins[i].month == month){
+            if(year){
+              if (Basins[i].year == year){
+                BasinsSeg.push(Basins[i]);
+              }
+            }else{
+              BasinsSeg.push(Basins[i]);
+            }
+          }
+        }else if(year){
+          if(Basins[i].year == year){
+                BasinsSeg.push(Basins[i]);
+          }
+        }else{
+          BasinsSeg.push(Basins[i]);
+        }
       }
     }
-    if(mes == 404){
-      res.sendStatus(mes);
+    if(BasinsSeg.length == 0){
+      res.sendStatus(cod);
     }else{
-      res.send(mes);
+      res.send(BasinsSeg);
     }
   }
 }
@@ -148,31 +193,31 @@ module.exports.postBasin = (req, res) => {
 }
 
 module.exports.putBasin = (req, res) => {
-  var pl = req.params.river_basin;
-  var plb = req.body;
-  var mes = 404;
+  var basin = req.params.river_basin;
+  var basinb = req.body;
+  var cod = 404;
   for(var i = 0; i < Basins.length; i++){
-    if(Basins[i].river_basin == pl){
+    if(Basins[i].river_basin == basin){
       Basins.splice(i, 1);
-      Basins.push(plb);
-      mes = 200;
+      Basins.push(basinb);
+      cod = 200;
       break;
     }
   }
-  res.sendStatus(mes);
+  res.sendStatus(cod);
 }
 
 module.exports.deleteBasin = (req, res) => {
-  var pl = req.params.river_basin;
-  var mes = 404;
+  var basin = req.params.river_basin;
+  var cod = 404;
   for(var i = 0; i < Basins.length; i++){
     if(Basins[i].river_basin == pl){
       Basins.splice(i, 1);
-      mes = 200;
+      cod = 200;
       break;
     }
   }
-  res.sendStatus(mes);
+  res.sendStatus(cod);
 }
 
 /*module.exports.loadInitialDataV1 = (req, res) => {
