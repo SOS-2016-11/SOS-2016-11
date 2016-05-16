ce289c256466a118b08f107f9cd7eac2c43a48b8
Pedro Ganga <pedrito_villa_16@hotmail.com>
2016-05-16 11:02:07 +0200

    vamos556

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ var governify = require("governify");
 var request = require('request');
 var cors = require('cors');
 //var port = (process.env.PORT || 16000);
-var port = (process.env.PORT ||10065);
+var port = (process.env.PORT ||14065);
 
 var app = express();
-app.use(bodyParser.json());
-app.use("/", express.static(__dirname + "/static"));
 
 //Governify
 //multiPlan_C2_sos-2016-11-pgt_ag
@@ -26,39 +24,29 @@ api.use(cors());
 api.get("/api",(req, res)=>{
   res.send({name:"pedro", number: 12344});
 });
-api.listen(17171);
+//api.listen(17171);
 
-//app.use(cors());
+app.use(bodyParser.json());
+app.use("/", express.static(__dirname + "/static"));
 
 var apisosCtl = require('./api/apisosCtl.js');
 var apicntcar = require("./api/apicntcar.js");
-
-app.get("/time", (req,res)=>{
-  days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-  var now = new Date();
-  var d = now.getDay();
-  var day = days[d];
-  var hour = ("0" + now.getHours()).slice(-2);
-  var minutes = ("0" + now.getMinutes()).slice(-2);
-  var seconds = ("0" + now.getSeconds()).slice(-2);
-  res.send("Today is  " + day + ", and are " + hour + ":" + minutes + ":" + seconds);
-});
-
 // API REST CARLOS /////////////////////////////////////////////////////////////
 // PROXY
+/*
 var paths='/api/v1/divorces-spanish/';
 var apiServerHost = 'http://sos-2016-10.herokuapp.com';
 app.use(paths, function(req, res) {
   var url = apiServerHost + req.baseUrl + req.url;
   console.log('piped: '+req.baseUrl + '///' +req.url);
-  req.pipe(request(url,(error, response, body)=>{
+  req.pipe(request(url,(error, response, body)=>{  */
   /*  if (error.code === 'ECONNREFUSED'){
       console.error('Refused connection');
     } else {
       throw error;
-    }
-  */})).pipe(res);
-});
+    }  */
+/*  })).pipe(res);
+});  */
 
 //Proxy (supongo que tendremos que comentar algunas partes para las distintas funcionalidades de cada integrante del grupo)
 var paths='/api/v1/pressure-and-temperatures?apikey=multiPlan_C2_sos-2016-11-pgt_ag';
@@ -76,9 +64,21 @@ app.use(paths, function(req, res) {
   })).pipe(res);
 });
 //Fin proxy
+app.get("/time", (req,res)=>{
+  days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  var now = new Date();
+  var d = now.getDay();
+  var day = days[d];
+  var hour = ("0" + now.getHours()).slice(-2);
+  var minutes = ("0" + now.getMinutes()).slice(-2);
+  var seconds = ("0" + now.getSeconds()).slice(-2);
+  res.send("Today is  " + day + ", and are " + hour + ":" + minutes + ":" + seconds);
+});
 
+// API REST CARLOS
 
 // SANDBOX
+
 app.get("/api/sandbox/programming_languages/", apicntcar.getPLs);
 
 app.post("/api/sandbox/programming_languages/", apicntcar.postPLs);
@@ -100,6 +100,7 @@ app.delete("/api/sandbox/programming_languages/:name", apicntcar.deletePL);
 app.get("/api-test/programming_languages/loadInitialData", apicntcar.loadInitialData);
 
 // V1
+
 app.get("/api/v1/average-rainfall/", apicntcar.getBasins);
 
 app.post("/api/v1/average-rainfall/", apicntcar.postBasins);
@@ -124,8 +125,6 @@ app.put("/api/v1/average-rainfall/:river_basin/:dat", apicntcar.putBasinDat);
 app.delete("/api/v1/average-rainfall/:river_basin", apicntcar.deleteBasin);
 
 app.delete("/api/v1/average-rainfall/:river_basin/:dat", apicntcar.deleteBasinDat);
-
-
 
 // API REST PEDRO
 
